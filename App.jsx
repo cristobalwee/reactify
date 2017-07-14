85e689abafd36a74e54b32fb1109fc7f16e93a8d
Cristobal Grana <cristobalgrana@Cristobals-MacBook-Pro.local>
2017-07-13 21:58:40 -0700

    initial commit

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import './styles.scss';
+import { TimelineMax, TweenMax } from 'gsap';
+import $ from 'jquery';
+
+class App extends Component {
+  render() {
+    return(
+      <div>Hello</div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+//https://stackoverflow.com/questions/42795946/trying-to-add-sass-to-a-react-application
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    HtmlWebpackPluginConfig
+  ]
+}
