ba6c9a6488331eeefe083b32ae19d59c7af5eba1
Cristobal Grana <cristobalgrana@Cristobals-MacBook-Pro.local>
2017-07-15 15:56:10 -0700

    Weback config

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,21 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      // { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      // { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.scss$/, loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
         })
+      },
+      {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          query:
+            {
+              presets:['react']
+            }
       }
     ]
   },
